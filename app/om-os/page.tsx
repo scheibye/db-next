0ceb0d5c1a1c89fb2d34c8bdabb9f31c9321059a
Andrei Hudovich <andrei@hudovich.com>
2025-12-02 17:37:25 +0100

    feat: add TextWithEmbeddedVideoSection

--- app/om-os/page.tsx
+++ app/om-os/page.tsx
@@ -2,12 +2,14 @@ import { CtaImagePatternSection } from '@/components/sections/CtaImagePatternSec
 import { CtaSmallSection } from '@/components/sections/CtaSmallSection'
 import { SimpleHero } from '@/components/sections/heros/SimpleHero'
 import { MissionSection } from '@/components/sections/MissionSection'
+import { TextWithEmbeddedVideoSection } from '@/components/sections/TextWithEmbeddedVideoSection'
 
 export default function AboutPage() {
   return (
     <>
       <SimpleHero label="Om os" title="Et enkelt og gennemsigtigt alternativ til bankens nej" />
-      <MissionSection />
+      <TextWithEmbeddedVideoSection />
+      <MissionSection className="py-16 lg:py-32" />
       <CtaSmallSection className="bg-brand-spring" />
       <CtaImagePatternSection imageSrc="/sections/cta-image-pattern/cta-image-pattern-bg-2.jpg" />
     </>
