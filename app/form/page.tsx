268956fabd53415761a0d87e9de259aaafaa4ca4
Andrei Hudovich <andrei@hudovich.com>
2025-12-10 16:59:54 +0100

    feat(form): set hero max height

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -8,10 +8,15 @@ import { SimpleHero } from '@/components/sections/heros/SimpleHero'
 export default function FormPage() {
   return (
     <>
-      <SimpleHero label="Dansk Boliglån" title="Få et gratis tilbud" withTrustpilot={true} />
+      <SimpleHero
+        className="hidden xl:block xl:max-h-120 2xl:max-h-144"
+        label="Dansk Boliglån"
+        title="Få et gratis tilbud"
+        withTrustpilot={true}
+      />
 
       {/* Z-index to be above the hero */}
-      <SectionContainer className="z-10 xl:pb-50" noPadding={true}>
+      <SectionContainer className="z-10 xl:pb-35" noPadding={true}>
         <SectionContainerInner>
           <LoanForm className="lg:col-start-2 lg:col-end-12 xl:-mt-32" />
         </SectionContainerInner>
